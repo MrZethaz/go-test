efae937b432876c8cca8314df31e94cc198ff09c
Apollo Daniel <developer.apollo.mail@gmail.com>
2025-04-29 17:22:43 -0300

    fix: fix function calls to new function names

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := Soma(15, 15)
+	total := soma(15, 15)
 	if total != 30 {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
@@ -12,7 +12,7 @@ func TestSoma(t *testing.T) {
 
 func TestSoma2(t *testing.T) {
 
-	total := Soma(15, 15)
+	total := soma(15, 15)
 	if total != 30 {
 		t.Errorf("Resultado da soma é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
